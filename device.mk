db8f49532d242e845695737d813f1f5f776af433
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    sweet: overlay: Configure aux camera for Aperture
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

--- device.mk
+++ device.mk
@@ -94,7 +94,8 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-awaken
+    $(LOCAL_PATH)/overlay-awaken \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Overlay-RRO
 PRODUCT_PACKAGES += \
