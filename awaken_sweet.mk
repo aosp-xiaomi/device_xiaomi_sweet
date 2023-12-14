30b6b808c3b15ce4590b824ea43777afe3b35048
lemezohaib <zhbkhan644@gmail.com>
2023-12-14 08:17:35 +0100

    sweet: Welcome AwakenOS
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- awaken_sweet.mk
+++ awaken_sweet.mk
@@ -11,10 +11,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Awaken stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Google Recorder
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Google Assistant
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+#Pixel Charger
+USE_PIXEL_CHARGER := true
+
+#official
+AWAKEN_BUILD_TYPE := official
+
+PRODUCT_NAME := awaken_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
