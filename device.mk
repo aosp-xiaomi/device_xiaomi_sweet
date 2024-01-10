e4818f46aa9461a43b444267327f7be096e01fd3
John Galt <johngaltfirstrun@gmail.com>
2024-01-10 14:23:21 +0000

    sweet: Add support for Automatic HBM. This is the way AOSP handles HBM in sunlight.
    Change-Id: I59c5ad75134944792ac4bb37fa8400fdecb91f3e
    
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- device.mk
+++ device.mk
@@ -37,7 +37,8 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml \
-    $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml
+    $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml \
+    $(LOCAL_PATH)/configs/display/display_id_4630946711218184577.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/displayconfig/display_id_4630946711218184577.xml
 
 PRODUCT_PACKAGES += \
     hwcomposer.qcom
