7097b7e23047b15e1633477ab13473e7199beeb8
basamaryan <basam.aryan@gmail.com>
2024-07-22 20:09:39 +0530

    sweet: Enable TARGET_USES_DOLBY
    Change-Id: I33d799f4a9f3b8fe05dbfcfa3d623049da187f00

--- device.mk
+++ device.mk
@@ -36,6 +36,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     hwcomposer.sweet
 
+# Dolby
+TARGET_USES_DOLBY := true
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.sweet.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.sweet.rc
