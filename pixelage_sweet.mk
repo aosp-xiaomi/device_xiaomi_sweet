00f41cac10e9ee27eba3a9d7c9867ff75aa7c595
lemezohaib <zhbkhan644@gmail.com>
2025-01-18 13:46:59 +0100

    sweet: Welcome Pixelage
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- pixelage_sweet.mk
+++ pixelage_sweet.mk
@@ -12,10 +12,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelage stuff.
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Pixelage
+PIXELAGE_BUILDTYPE := OFFICIAL
+PIXELAGE_MAINTAINER := Zohaib
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_NAME := pixelage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
