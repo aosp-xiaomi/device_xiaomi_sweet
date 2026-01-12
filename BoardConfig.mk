907858404ab688a6518dd7298c41bcf795953662
lemezohaib <zhbkhan644@gmail.com>
2026-01-13 01:02:45 +0500

    fixup! sweet: Move to vendor/oneplus/dolby
    Change-Id: I6b506928eff4618501563f2d2527119e554164d2
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,9 +7,6 @@
 # Inherit from sm6150-common
 include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
-# Inherit from Dolby
--include device/oneplus/dolby/BoardConfig.mk
-
 # MiuiCamera
 -include device/xiaomi/miuicamera-sweet/BoardConfig.mk
 
--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
 # Inherit from dolby
-$(call inherit-product-if-exists, device/oneplus/dolby/device.mk)
+$(call inherit-product-if-exists, vendor/oneplus/dolby/oplusdolby.mk)
 
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-sweet/device.mk)
